91294eb8fe77952b49416d80af714ded36cc4e46
Damian Oswald <72310532+Damian-Oswald@users.noreply.github.com>
2023-11-14 17:52:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,252 @@
+
+# Milch Price Forecasting Model
+
+The Milk Price Forecasting Model is a pilot project of the Federal
+Office for Agriculture. It is implemented in an R script. It integrates
+various statistical and machine learning models, offering a detailed
+analysis of milk price trends and predictions.
+
+## User Guide
+
+The excel file `milk-price-data.xlsx` with the relevant data must be
+placed in the `data` directory for the script to function correctly.
+Note that future values may be added directly to that table, but header
+names must remain the same. Running the script in an R environment will
+trigger a series of processes including data preprocessing, model
+training and evaluation, and the generation of visualizations and
+reports.
+
+The output from the script is multi-faceted: two PDF reports will be
+written into a directory `reports`. The file
+`time-series-decomposition.pdf` summarizes the data and visualizes the
+seasonal decomposition of each variable. The file
+`machine-learning-report.pdf` summarizes the statistical models applied
+and their respective forecastings. In addition, various tables with
+numerical results are written into the directory `tables`.
+
+<div>
+
+<table style="width:100%;">
+<colgroup>
+<col style="width: 33%" />
+<col style="width: 33%" />
+<col style="width: 33%" />
+</colgroup>
+<tbody>
+<tr class="odd">
+<td style="text-align: center;"><div width="33.3%"
+data-layout-align="center">
+<figure>
+<img src="images/xlsx.svg" data-fig.extended="false" width="80"
+alt="correlations.xlsx" />
+<figcaption
+aria-hidden="true"><code>correlations.xlsx</code></figcaption>
+</figure>
+</div></td>
+<td style="text-align: center;"><div width="33.3%"
+data-layout-align="center">
+<figure>
+<img src="images/xlsx.svg" data-fig.extended="false" width="80"
+alt="correlations.xlsx" />
+<figcaption
+aria-hidden="true"><code>correlations.xlsx</code></figcaption>
+</figure>
+</div></td>
+<td style="text-align: center;"><div width="33.3%"
+data-layout-align="center">
+<figure>
+<img src="images/xlsx.svg" data-fig.extended="false" width="80"
+alt="correlations.xlsx" />
+<figcaption
+aria-hidden="true"><code>correlations.xlsx</code></figcaption>
+</figure>
+</div></td>
+</tr>
+</tbody>
+</table>
+
+<table style="width:67%;">
+<colgroup>
+<col style="width: 33%" />
+<col style="width: 33%" />
+</colgroup>
+<tbody>
+<tr class="odd">
+<td style="text-align: center;"><div width="33.3%"
+data-layout-align="center">
+<figure>
+<img src="images/pdf.svg" data-fig.extended="false" width="60"
+alt="machine-learning-report.pdf" />
+<figcaption
+aria-hidden="true"><code>machine-learning-report.pdf</code></figcaption>
+</figure>
+</div></td>
+<td style="text-align: center;"><div width="33.3%"
+data-layout-align="center">
+<figure>
+<img src="images/pdf.svg" data-fig.extended="false" width="60"
+alt="time-series-decomposition.pdf" />
+<figcaption
+aria-hidden="true"><code>time-series-decomposition.pdf</code></figcaption>
+</figure>
+</div></td>
+</tr>
+</tbody>
+</table>
+
+Figure 1: Overview of the different files written by the R script
+`script.R`.
+
+</div>
+
+This documentation encapsulates the script’s capabilities, underlying
+principles, and offers guidance for users to leverage its
+functionalities effectively.
+
+# How The Script Works
+
+## Setup
+
+The script begins by setting up the necessary data environment, using
+`milk-price-data.xlsx` as its primary data source. It specifically
+targets `CH_Milchpreis` for prediction, reserving the final 18 months of
+data for testing. This number may be adjusted via the variable
+`test_set_size`.
+
+The script incorporates a broad spectrum of features, encompassing
+various milk price types and related economic indicators, allowing for a
+comprehensive analysis.
+
+## Seasonal Decomposition
+
+This part of the script breaks down the time-series data into trend,
+seasonal, and residual elements, offering valuable insights into the
+underlying patterns that govern milk price changes. It repeats this with
+every single variable in the data, so long seasonal effects are detected
+([Figure 2](#fig-seasonal)).
+
+<figure>
+<img src="images/decomposition.png" id="fig-seasonal"
+alt="Figure 2: Example of a seasonal decomposition plot for CH_Milchpreis. The milk price is decomposed into a trend (via a moving average), a seasonal effect and the remainder. The bars on the right indicate the relative magnitude of the effects." />
+<figcaption aria-hidden="true">Figure 2: Example of a seasonal
+decomposition plot for <code>CH_Milchpreis</code>. The milk price is
+decomposed into a trend (via a moving average), a seasonal effect and
+the remainder. The bars on the right indicate the relative magnitude of
+the effects.</figcaption>
+</figure>
+
+## Data Analysis and Visualization
+
+Data visualization and analysis are integral parts of the script. It
+includes comprehensive code segments for visually representing the
+performance of various models and their forecast accuracy. This visual
+comparison is crucial for understanding the effectiveness of each model.
+All plots generated by the script are written into the file
+`machine-learning-report.pdf`.
+
+The script also features a time-series decomposition component. This
+section breaks down the time-series data into trend, seasonal, and
+residual elements, offering valuable insights into the underlying
+patterns that govern milk price changes.
+
+## Milk Price Forecasting
+
+For the milk price forecasting, four different models are fit to the
+data: A lasso regression model, a ridge regression model, an ARIMA model
+as well as a SARIMA model.
+
+### Data preparation
+
+First, the data is prepared: The `time` feature is replaced by
+respective sine and cosine transformations. The target variable,
+i.e. `CH_Milchpreis`, is lagged with different forecast horizons,
+`h = 1`, `h = 2`, and `h = 3`. All `NA` values in the features are
+replaced by zero – except for the ones created by lagging the target
+variable.
+
+Next, all original features are z-score normalized. This ensures a more
+reliable model fitting and also equivalent variable weight.
+
+The prepared data is split into training and testing sets. The training
+set consists of all data up until `test_set_size = 18` months before the
+last entry. These observations are used to calibrate the model.
+Correspondingly, the latest `test_set_size = 18` observations are used
+to test the model.
+
+### Forecasting with lasso and ridge regression
+
+Generally speaking, any forecasting model $f$ predicts a future value of
+$X$, say $X_{t+1}$, based on a past value $X_t$. Autoregressive models
+predict following values based on the predictions already made
+recursively. For the ridge and lasso regression models, this is not
+possible: They only predict the Swiss milk price (denoted as
+$X_{1,t+h}$), while needing many other values
+([Equation 1](#eq-forecasting)). This means, after one round of
+predicting, these other values are missing for continueing
+autoregressively.
+
+<span id="eq-forecasting">$$
+f: X_{1,t+h} = \beta_0 + \sum_{i=1}^p \beta_i X_{i,t}
+ \qquad(1)$$</span>
+
+For the purpose of predicting milk prices more than one month into the
+future, three different models are trained – each with a different
+forecast horizons $h$. Consequently, three instances per model can be
+compared with each other in the end.
+
+The hyperparameter $\lambda$ which is penalizing large coefficients, is
+selected via cross-validation with the function `cv.glmnet` from the
+`glmnet` package. In order to prevent overfitting, the folds are chosen
+as six-month batches instead of randomly.
+
+### Forcasting with (seasonal) ARIMA
+
+As the name says, the autoregressive integrated moving average (ARIMA)
+model is autoregressive. Thus, it can forecast values for how many
+periods as we want it to. For the purpose of comparing ARIMA and SARIMA
+models with the regularized models, the same forecast horizon is chosen.
+
+### Model evaluation
+
+# Details About the Implemented Models
+
+A significant part of the script is dedicated to implementing and
+fine-tuning different statistical models: two regularization models
+(Ridge and Lasso regression), and an autoregressive integrated moving
+average (ARIMA) model, both with and without seasonal effects.
+
+## Regularized Least Squares
+
+Ridge and Lasso regression are especially effective in handling
+multicollinearity and preventing overfitting. Both models are linear in
+nature, i.e. the response variable $\mathbf y$ can be expressed as a
+linear combination of predictors $\mathbf X \boldsymbol \beta$ plus some
+residual $\boldsymbol \varepsilon$.
+
+<span
+id="eq-linear">$$\mathbf y = \mathbf X \boldsymbol \beta + \boldsymbol \varepsilon \qquad(2)$$</span>
+
+Ridge Regression introduces an $\ell_2$-penalty proportional to the
+square of the coefficient magnitudes. On the other side, Lasso
+Regression employs an $\ell_1$-penalty, encouraging sparser solutions –
+only a few predictors are selected.
+
+<span
+id="eq-ridge">$$\boldsymbol {\hat \beta}_{\text{ridge}} = \min_{\boldsymbol \beta} \left\{ \| \mathbf y - \mathbf X \boldsymbol \beta \|_2^2 + \lambda \| \boldsymbol \beta\|_2^2 \right\} \qquad(3)$$</span>
+
+<span
+id="eq-ridge">$$\boldsymbol {\hat \beta}_{\text{lasso}} = \min_{\boldsymbol \beta} \left\{ \| \mathbf y - \mathbf X \boldsymbol \beta \|_1 + \lambda \| \boldsymbol \beta\|_2^2 \right\} \qquad(4)$$</span>
+
+The mathematical formulations for these regressions are centered around
+minimizing the sum of squared residuals, with added regularization terms
+($\ell_2$-norm for Ridge and $\ell_1$-norm for Lasso).
+
+## Autoregressive Integrated Moving Average
+
+Additionally, the script employs an Autoregressive Integrated Moving
+Average (ARIMA) and its seasonal variant (SARIMA), which are both
+pivotal for time-series forecasting. ARIMA is most suitable for
+non-seasonal data and combines autoregressive and moving average
+components. In contrast, SARIMA extends this to accommodate seasonal
+fluctuations in data, making it more robust for datasets with seasonal
+trends.
---
+++ script.R
@@ -0,0 +1,289 @@
+#===============================================================================================================
+# title: Milch price forecasting model
+# author: Damian Oswald
+# date: 2023-11-08
+#===============================================================================================================
+
+
+#===============================================================================================================
+# Setting up work space
+#===============================================================================================================
+
+# user inputs
+name_of_the_data <- "milk-price-data.xlsx"
+
+# define target variable
+target <- "CH_Milchpreis"
+
+# how many months of the data set should be used for the test set?
+test_set_size <- 18
+
+# define feature names
+features <- c("TotMilch_Menge_inkl_LI", "CH_Milchpreis", "Molkerei_Milchpreis",
+              "Verkaest_Milchpreis", "Gewerblich_Milchpreis", "Bio_Milchpreis",
+              "EU_Milchpreis_CHF", "Kurs_CHF_Euro", "Total_Staatlich_Zahlung",
+              "staatlich_kg_Milch", "Kontingent_01", "Freihandel_01", "EU_Politik_01",
+              "Industriell_verkäst_Milchpreis", "Import_Menge_Käse_und_Quark_kg",
+              "Schlachtungen_Kühe", "Magermilchpulver_Prod_Menge",
+              "Vollmilchpulver_Prod_Menge", "Butter_Prod_Menge",
+              "Export_Menge_Käse_und_Quark_kg", "Milchpreis_NZ_Euro")
+
+# conditional installation of packages
+for (package in c("forecast", "ggplot2", "tidyr", "GGally", "tibble", "magrittr", "readxl", "visdat", "seastests", "ggcorrplot", "openxlsx", "glmnet")) {
+  if(!do.call(require,list(package))) install.packages(package)
+  do.call(library,list(package))
+}
+
+# Forecasting using Ridge Regression
+cat("\f\nFederal Office of Agriculture (2023)\n___  ____ _ _     ______     _           ______                           _   _             \n|  \\/  (_) | |    | ___ \\   (_)          |  ___|                         | | (_)            \n| .  . |_| | | _  | |_/ / __ _  ___ ___  | |_ ___  _ __ ___  ___ __ _ ___| |_ _ _ __   __ _ \n| |\\/| | | | |/ / |  __/ '__| |/ __/ _ \\ |  _/ _ \\| '__/ _ \\/ __/ _` / __| __| | '_ \\ / _` |\n| |  | | | |   <  | |  | |  | | (_|  __/ | || (_) | | |  __/ (_| (_| \\__ \\ |_| | | | | (_| |\n\\_|  |_/_|_|_|\\_\\ |_|  |_|  |_|\\___\\___  |_| \\___/|_|  \\___|\\___\\__,_|___/\\__|_|_| |_|\\__, |\n                                                                                       __/ |\n                                                                                      |___/\n")
+
+# read the data
+data <- file.path("data", name_of_the_data) |>
+  readxl::read_excel() |>
+  as.data.frame() |>
+  subset(select = features)
+data_scaled <- scale(data)
+data_ts <- ts(data, start = 2000, frequency = 12)
+
+
+#===============================================================================================================
+# Decomposing trend, seasonal effects and errors from every feature
+#===============================================================================================================
+
+# function to convert a decomposed time series object to a data frame
+ts2df <- function(x) {
+  d <- decompose(x)
+  data.frame(year = floor(time(x)), month = as.integer(cycle(x)),
+             observation = d$x,
+             trend = d$trend,
+             seasonal = seasonal(d),
+             remainder = d$random)
+}
+
+# conditional creation of folder
+if(!file.exists(file.path(getwd(),"reports"))) dir.create(file.path(getwd(),"reports"))
+if(!file.exists(file.path(getwd(),"tables"))) dir.create(file.path(getwd(),"tables"))
+
+# create empty pdf
+pdf(file.path(getwd(),"reports","time-series-decomposition.pdf"), width = 16, height = 9)
+
+# title slide
+plot.new()
+text(0, 1, paste("This PDF contains results from an automated time-series analysis report."), pos = 4, xpd = NA)
+text(0, 0.95, Sys.time(), pos = 4, xpd = NA)
+text(0, 0.9, version$version.string, pos = 4, xpd = NA)
+
+# missing values
+data_ts |> as.data.frame() |> visdat::vis_miss() |> print()
+
+# correlation plot
+data_ts |> cor(use = "complete.obs") |> ggcorrplot::ggcorrplot(method = "circle", type = "lower", outline.color = par()$fg, lab_size = 2, lab = TRUE) |> print()
+write.table(x = cor(data_ts, use = "complete.obs"), file = file.path("tables","correlations.csv"), sep = ";")
+
+# decomposition plots
+for (i in 1:length(features)) {
+  cat("\rDecomposing time series [", i, "/", length(features),"]", sep = "")
+  if(!seastests:::isSeasonal(data_ts[,features[i]])) {
+    data_ts[,features[i]] |> forecast:::autoplot.ts(ylab = "") + ggtitle(paste(features[i], ": Time series plot"), subtitle = paste0("Ollech and Webel's combined seasonality test didn't detect seasonality in this data (p-value = ",paste(round(max(seastests::combined_test(data_ts[,features[i]])$Pval),3),collapse = ", "),")")) -> p
+    print(p)
+  } else {
+    data_ts[,features[i]] |> decompose() -> decomposition
+    decomposition |> forecast:::autoplot.decomposed.ts() + ggtitle(paste(features[i],": Decomposition of additive time series"), "Time series decomposition into seasonal, trend and irregular components using moving averages.") -> p
+    print(p + theme_minimal())
+  }
+}
+
+# close pdf
+dev.off()
+
+# Write all the results into one excel sheet
+list <- lapply(features, function(i) ts2df(data_ts[,i]))
+names(list) <- features
+write.xlsx(list, file = file.path("tables","decomposition.xlsx"))
+
+#===============================================================================================================
+# Prepare data for forecasting
+#===============================================================================================================
+
+cat("\nPreparing data")
+
+# sine and cosine transformer
+sinCosTransform <- function(x, frequency = 1) {
+  cbind(sin = sin(x * 2 * pi/frequency), cos = cos(x * 2 * pi/frequency))
+}
+
+# function to add lag to a numeric vector x (can generate multiple lags at the same time)
+lag <- function(x, k = 1, col.names = NA) {
+  Y <- sapply(k, function(k) {
+    n <- length(x)
+    c(x[(1+k):n], rep(NA, k)) 
+  })
+  if(!is.na(col.names)) colnames(Y) <- col.names
+  else colnames(Y) <- paste0("y", k)
+  return(Y)
+}
+
+# add transformations and labels (with a lag)
+df <- cbind(lag(data_ts[,target], k = 1:3),
+            time = as.numeric(time(data_ts)),
+            sinCosTransform(as.numeric(time(data_ts))),
+            as.matrix(as.data.frame(data_ts)))
+
+# replace all NA value by zero
+df[,features][is.na(df[,features])] <- 0
+
+# scale the data
+df <- scale(df)
+df[,"time"] <- df[,"time"]*sd(as.numeric(time(data_ts)))+mean(as.numeric(time(data_ts))) # keep time or
+
+# function to re-scale scaled values to the original size
+rescale <- function(z, mu = mean(data[,target]), sigma = sd(data[,target])) sigma*z + mu
+
+# test-train-split (keep the last year as a test dataset)
+df_train <- head(df, -test_set_size)
+df_test <- tail(df, test_set_size)
+
+# In the train dataset, divide folds for cross validation as six-month periods 
+foldid <- (df_train[,"time"] %/% 0.5 - 3999)
+
+# Save all features allowed to predict the model in one vector
+features <- c("sin","cos",features)
+label <- "y1"
+
+#===============================================================================================================
+# Forecasting the milk price with lasso and ridge regression
+#===============================================================================================================
+
+# we want to forecast 1, 2 and 3 months into the future
+for (i in 3:1) {
+  
+  # print message
+  cat("\rFitting regularized least squares [", (4-i), "/3]", sep = "")
+  
+  # traine one L1 and one L2 regularized least squares model (lasso and ridge)
+  L1 <- glmnet::cv.glmnet(x = df_train[,features], y = df_train[,paste0("y",i)], lambda = exp(seq(-12,0,0.25)), alpha = 1, foldid = foldid)
+  L2 <- glmnet::cv.glmnet(x = df_train[,features], y = df_train[,paste0("y",i)], lambda = exp(seq(-12,0,0.25)), alpha = 0, foldid = foldid)
+  
+  # visualize the coefficients as a coefficients plot
+  coefficients <- data.frame(Variable = rep(rownames(coefficients(L1)), 2), Coefficient = c(as.numeric(coef(L1)),as.numeric(coef(L2))), Norm = rep(c("L1", "L2"), each = length(coef(L2))))
+  coefficients$Variable <- factor(coefficients$Variable, levels = rownames(coefficients(L2))[order(abs(rowSums(cbind(coef(L1), coef(L2)))))])
+  highlight <- coefficients[coefficients$Variable %in% rownames(coef(L1))[which(abs(coef(L1))>0)],]
+  assign(paste0("plot_coefficients_",paste0("y",i)),
+         ggplot2::ggplot(coefficients, aes(Coefficient, Variable)) +
+           geom_line(aes(group = Variable), alpha = 0.3) + geom_point(aes(color = Norm), size = 2, alpha = 0.5) +
+           geom_line(data = highlight, aes(group = Variable)) + geom_point(data = highlight, aes(color = Norm), size = 3)
+  )
+  
+  # save predictions
+  assign(paste0("predictions_",paste0("y",i)), {
+    data.frame(
+      Lasso = as.numeric(predict(L1, newx = as.matrix(df_test[,features]))),
+      Ridge = as.numeric(predict(L2, newx = as.matrix(df_test[,features])))
+    )
+  })
+}
+
+#===============================================================================================================
+# Forecasting the milk price with (seasonal) ARIMA
+#===============================================================================================================
+
+# find ARIMA(1,1,1) model to fit the data
+predictions_arima <- matrix(NA, test_set_size, 3)
+cat("\n")
+for (t in 1:test_set_size) {
+  cat("\rFitting ARIMA [", t, "/", test_set_size, "]", sep = "")
+  df[1:(nrow(df)-t),target] |>
+    arima(order = c(1,1,1)) |>
+    predict(n.ahead = 3, se.fit = FALSE) -> predictions_arima[test_set_size+1-t,]
+}
+
+# find ARIMA(1,1,1)(1,1,1)[12] model to fit the 
+predictions_sarima <- matrix(NA, test_set_size, 3)
+cat("\n")
+for (t in 1:test_set_size) {
+  cat("\rFitting SARIMA [", t, "/", test_set_size, "]", sep = "")
+  df[1:(nrow(df)-t),target] |>
+    arima(order = c(1,1,1), seasonal = list(order = c(1,1,1), period = 12)) |>
+    predict(n.ahead = 3, se.fit = FALSE) -> predictions_sarima[test_set_size+1-t,]
+}
+
+# Combine with RLS predictions
+predictions_y1 <- cbind(predictions_y1, ARIMA = predictions_arima[,1], SARIMA = predictions_sarima[,1])
+predictions_y2 <- cbind(predictions_y2, ARIMA = predictions_arima[,2], SARIMA = predictions_sarima[,2])
+predictions_y3 <- cbind(predictions_y3, ARIMA = predictions_arima[,3], SARIMA = predictions_sarima[,3])
+
+# Combine all forecastings (with unseen observations to compare)
+t <- max(df[,"time"]) + c(1/12, 2/12, 3/12)
+forecastings <- rescale(rbind(tail(predictions_y1, 1), tail(predictions_y2, 1), tail(predictions_y3, 1)))
+write.xlsx(data.frame(Year = t%/%1, Month = t%%1*12+1, forecastings), file = file.path("tables","predictions.xlsx"))
+
+
+##===============================================================================================================
+# Testing all models
+#===============================================================================================================
+
+# calculate root mean squared error (RMSE)
+rmse <- function(predictions, observations) {
+  Errors <- (rescale(predictions) - rescale(observations))
+  sqrt(colMeans(Errors^2, na.rm = TRUE))
+}
+RMSE <- sapply(1:3, function(i) rmse(get(paste0("predictions_y",i)), df_test[,paste0("y",i)])) |> t()
+RMSE <- data.frame(time = rep(as.character(1:3), times = 4), gather(as.data.frame(RMSE)))
+
+# make plot of all model performances
+overall_comparison <- ggplot(data = RMSE, aes(x = key, y = value, fill = time)) +
+  ylab("Root mean squared error [CHF]") +
+  geom_bar(stat = "identity", position=position_dodge()) +
+  geom_text(aes(label = round(value, 2)), color = "black", position = position_dodge(0.9), vjust = -1) +
+  scale_fill_brewer(palette = "Reds") +
+  ggtitle("Model performance comparison",paste("This chart shows the root mean squared error (RMSE), which represents the average error, of different models.\nThe models were all tested on the milk price data of the past",test_set_size,"months, but trained without that data."))
+
+#===============================================================================================================
+# Export all plots to one PDF
+#===============================================================================================================
+
+# print out message
+cat("\nGenerate report [1/1]")
+
+# Export all plots as one PDF
+pdf(file.path("reports","machine-learning-report.pdf"), width = 16, height = 9)
+
+# Forecast of unseen values
+plot(x = df_test[,"time"], y = rescale(df_test[,target]), xlim = range(df_test[,"time"]) + c(0, 1/2), pch = 16, type = "o", las = 1, lwd = 3, ylab = "Swiss milk price", xlab = "Time")
+points(y = tail(predictions_y1,1), x = rep(max(df_test[,"time"]),ncol(predictions_y1)))
+grid(col=1, lty = 1)
+last <- max(df_test[,"time"])
+colors = c(ARIMA = "blue", SARIMA = "blue", Lasso = "red", Ridge = "red")
+for (model in colnames(forecastings)) {
+  lines(x = max(df_test[,"time"]) + c(1/12, 2/12, 3/12), y = forecastings[,model], lwd = 3, col = colors[model])
+  text(x = max(df_test[,"time"]) + 3/12, y = forecastings[3,model], labels = model, col = colors[model], pos = 4)
+}
+
+# Model comparison
+print(overall_comparison + theme_minimal())
+
+# Pais panel
+for(i in 1:3) {
+  cbind(Observation = df_test[,paste0("y",i)], get(paste0("predictions_",paste0("y",i)))) |>
+    na.omit() |>
+    rescale() |>
+    ggpairs(lower = list(continuous = "smooth"), progress = FALSE) +
+    theme_minimal() +
+    ggtitle(paste0("Comparing the forecast values (t + ",i,")"), subtitle = paste("These plots compare the forecast milk prices with the observed ones of the past", test_set_size, "months.")) -> plot
+  print(plot)
+}
+
+# Coefficients of lasso and ridge regression
+for (i in 1:3) print(get(ls()[grep("plot_coefficients",ls())][i]) + ggtitle(paste0("Visualization of the regularized least squares coefficients (t + ", i,")"), subtitle = "This coefficients are retrieved from the best-fit models trained with the L1-norm (lasso regression) and L2-norm (ridge regression). They indicate relative variable importance.") + theme_minimal())
+
+# Details of lasso and ridge
+par(mfrow = c(2,2), mar = c(4,4,5,1)+0.1)
+plot(L1, col = "black"); mtext("Lasso regression (L1)", line = 3, font = 2); abline(v = L1$lambda.min)
+plot(L2); mtext("Ridge regression (L2)", line = 3, font = 2)
+(L1$glmnet.fit) |> plot(col = "red", xvar = "lambda"); mtext("Lasso regression (L1)", line = 3, font = 2); abline(v = L1$lambda.min)
+(L2$glmnet.fit) |> plot(col = "red", xvar = "lambda"); mtext("Ridge regression (L2)", line = 3, font = 2)
+
+dev.off()
+
+cat("\n\nDone!")
+rm(list = ls())
